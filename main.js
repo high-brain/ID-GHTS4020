d9ab70daf15334d156970eddcf90412b3ba6d0b2
high-brain <128629631+high-brain@users.noreply.github.com>
2023-11-29 13:36:57 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+/* Set the width of side navigation to 250px as I said in the css section*/
+ /*
+function openNav(){
+  document.getElementById("mySidenav").style.width ="250px";
+}
+function closeNav(){
+  document.getElementById("mySidenav").style.width="0";
+}
+
+function openNav(){
+  document.getElementById("mySidenav").style.width ="250px";document.body.style.backgroundColor ="rgb(0,0,0,0,4)";
+}
+function closeNav(){
+  document.getElementById("mySidenav").style.width="0";
+  document.body.style.backgroundColor="white";
+}
+
+function openNav(){
+  document.getElementById("mySidenav").style.width="100%";
+}
+
+function closeNavNav(){
+  document.getElementById("mySidenav").style.width="0";
+}
+
+function openNav(){
+  document.getElementById("mySidenav").style.display="block";
+}
+
+function closeNavNav(){
+  document.getElementById("mySidenav").style.display="none";
+}
+
+*/
